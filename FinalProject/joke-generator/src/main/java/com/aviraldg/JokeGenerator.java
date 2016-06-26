b4f9150f6a958b4c33006ff174213dc381db4a5e
Aviral Dasgupta <me@aviraldg.com>
2016-06-26 07:17:38 +0530

    feat: add Java library for joke generation

---
+++ FinalProject/joke-generator/src/main/java/com/aviraldg/JokeGenerator.java
@@ -0,0 +1,7 @@
+package com.aviraldg;
+
+public class JokeGenerator {
+    public static String getJoke() {
+        return "This is supposed to be a joke. It has no characters in it. Or does it?";
+    }
+}
