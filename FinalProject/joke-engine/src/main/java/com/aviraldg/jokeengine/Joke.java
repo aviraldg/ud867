8915a8950c4e1be5e1aceca9848ba7c9d14aaf88
Aviral Dasgupta <me@aviraldg.com>
2016-06-26 13:05:05 +0530

    feat: add GCE module to supply jokes

---
+++ FinalProject/joke-engine/src/main/java/com/aviraldg/jokeengine/Joke.java
@@ -0,0 +1,16 @@
+package com.aviraldg.jokeengine;
+
+/**
+ * The object model for the data we are sending through endpoints
+ */
+public class Joke {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ FinalProject/joke-engine/src/main/java/com/aviraldg/jokeengine/JokeEndpoint.java
@@ -0,0 +1,41 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.aviraldg.jokeengine;
+
+import com.aviraldg.JokeGenerator;
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "joke",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "jokeengine.aviraldg.com",
+                ownerName = "jokeengine.aviraldg.com",
+                packagePath = ""
+        )
+)
+public class JokeEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "getJoke")
+    public Joke getJoke() {
+        Joke response = new Joke();
+        response.setContent(JokeGenerator.getJoke());
+
+        return response;
+    }
+
+}
